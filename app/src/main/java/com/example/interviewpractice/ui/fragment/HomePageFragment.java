37ebe04fca60bf8f0b8e7ad1bdef93998eaa18fc
yuwoquan <gdvictor_ywq@126.com>
2017-12-27 16:14:51 +0800

    更改

--- app/src/main/java/com/example/interviewpractice/ui/fragment/HomePageFragment.java
+++ app/src/main/java/com/example/interviewpractice/ui/fragment/HomePageFragment.java
@@ -1,10 +1,12 @@
 package com.example.interviewpractice.ui.fragment;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.interviewpractice.R;
 import com.example.interviewpractice.enity.BannerBean;
@@ -12,6 +14,7 @@ import com.example.interviewpractice.presenter.BannerPresenterImp;
 import com.example.interviewpractice.ui.baseView.BaseFragment;
 import com.example.interviewpractice.utils.GlideImageLoader;
 import com.example.interviewpractice.view.BannerView;
+import com.example.interviewpractice.weight.IndicatorView;
 import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
 
@@ -25,8 +28,15 @@ public class HomePageFragment extends BaseFragment implements BannerView {
 
     @BindView(R.id.banner)
     Banner banner;
+    @BindView(R.id.main_home_entrance_vp)
+    ViewPager entranceViewPage;
+    @BindView(R.id.main_home_entrance_indicator)
+    IndicatorView entranceIndicatorView;
+    @BindView(R.id.home_entrance)
+    LinearLayout homeEntranceLayout;
 
-    private BannerPresenterImp bannerPresenterImp=new BannerPresenterImp(this,getContext());
+    private BannerPresenterImp bannerPresenterImp = new BannerPresenterImp(this, getContext());
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home_page, container, false);
@@ -35,19 +45,6 @@ public class HomePageFragment extends BaseFragment implements BannerView {
         return view;
     }
 
-
-    public void onStart() {
-        super.onStart();
-        //开始轮播
-        banner.startAutoPlay();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        //结束轮播
-        banner.stopAutoPlay();
-    }
 
     @Override
     public void showProgress() {
@@ -58,13 +55,15 @@ public class HomePageFragment extends BaseFragment implements BannerView {
     public void disimissProgress() {
 
     }
+
     @Override
     public void loadDataError(Throwable throwable) {
-            bannerPresenterImp.requestError(throwable);
+        bannerPresenterImp.requestError(throwable);
     }
+
     @Override
     public void loadDataSuccess(BannerBean tData) {
-        Log.e("123", "loadDataSuccess: "+tData.toString() );
+        Log.e("123", "loadDataSuccess: " + tData.toString());
         if (tData.getItemList() != null) {
             List<String> listImage = new ArrayList<>();
             List<String> listTitle = new ArrayList<>();
@@ -84,5 +83,22 @@ public class HomePageFragment extends BaseFragment implements BannerView {
         }
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        banner.stopAutoPlay();
+    }
 
+    public void onStart() {
+        super.onStart();
+        //开始轮播
+        banner.startAutoPlay();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        //结束轮播
+        banner.stopAutoPlay();
+    }
 }
