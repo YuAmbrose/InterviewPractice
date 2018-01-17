23063719e2788eaeaaf039cfe663bed8bf2a5277
yuwoquan <gdvictor_ywq@126.com>
2018-01-17 14:27:34 +0800

    使用view背景颜色来填充掉topbar，android:fitsSystemWindows="true"无效QMUI在多fragment会出现这个错误

--- app/src/main/java/com/example/interviewpractice/ui/fragment/DiscoverFragment.java
+++ app/src/main/java/com/example/interviewpractice/ui/fragment/DiscoverFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.interviewpractice.R;
 import com.example.interviewpractice.adapter.adapter.RankListFragmentAdapter;
@@ -21,6 +22,8 @@ public class DiscoverFragment extends BaseFragment {
 
     @BindView(R.id.topbar)
     QMUITopBar mTopBar;
+    @BindView(R.id.statebar)
+    View statebarHeight;
 //    @BindView(R.id.ranklisttablayout)
 //    TabLayout ranklisttablayout;
 //    @BindView(R.id.ranklistviewpager)
@@ -33,6 +36,11 @@ public class DiscoverFragment extends BaseFragment {
         View view = inflater.inflate(R.layout.fragment_discover, container, false);
         ButterKnife.bind(this, view);
         mTopBar.setTitle("发现");
+        LinearLayout.LayoutParams params= (LinearLayout.LayoutParams) statebarHeight.getLayoutParams();//获取当前控件的布局对象
+        params.height=getStatusBarHeight(this);//设置当前控件布局的高度
+        statebarHeight.setLayoutParams(params);//将设置好的布局参数应用到控件中
+
+
 //        rankListFragmentAdapter = new RankListFragmentAdapter(getFragmentManager());
 //        ranklistviewpager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
 //            @Override
@@ -48,13 +56,15 @@ public class DiscoverFragment extends BaseFragment {
 
     @Override
     public void onPause() {
-        Log.e(TAG, "onPause: " );
+        Log.e(TAG, "onPause: ");
         super.onPause();
     }
 
     @Override
     public void onStop() {
-        Log.e(TAG, "onStop: " );
+        Log.e(TAG, "onStop: ");
         super.onStop();
     }
+
+
 }
