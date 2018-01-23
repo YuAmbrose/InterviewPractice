0429056dd4f7e3d1f0b3091f44c8bb58385af779
yuwoquan <841453858@qq.com>
2018-01-23 23:16:10 +0800

    完善主页面。

---
+++ app/src/main/java/com/example/interviewpractice/v_layout/holder/EndHolder.java
@@ -0,0 +1,27 @@
+package com.example.interviewpractice.v_layout.holder;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.interviewpractice.R;
+import com.example.interviewpractice.enity.BannerBean;
+import com.example.interviewpractice.v_layout.VlayoutBaseHolder;
+
+import butterknife.BindView;
+
+/**
+ * Created by 鱼握拳 on 2018/1/23.
+ */
+
+public class EndHolder extends VlayoutBaseHolder<BannerBean> {
+
+public EndHolder(View itemView) {
+        super(itemView);
+        }
+
+@Override
+public void setData(int ps,BannerBean mData) {
+        super.setData(ps, mData);
+        }
+
+        }
